3be9cb9cc156e430e5eadc0facf7883e9b94f970
Ankush pal <palankush2007@gmail.com>
2026-05-21 13:18:43 +0530

    Initial commit

---
+++ conditional.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+// int num;
+// cout<<"Enter a number:";
+// cin>>num;
+// if(num==0){
+//     cout<<"Zero";
+// }else if(num>0){
+//     cout<<"positive number";
+// }else{
+//     cout<<"negative number \n" ;
+// }
+
+// int year;
+// cout<<"Enter a year:";
+// cin >> year;
+// if(year%400==0){
+//     cout<<"leap year";
+// }else if(year%100==0){
+//     cout<<"not a leap year ";
+// }else if (year%4==0){
+//     cout<<"leap year 4 ";
+// }else {
+//     cout<< year << "not a leap year";
+// }
+// if((year%4==0 && year%100!=0) ||(year%400==0)){
+//     cout<< year << "leap year";
+// }else{cout<<"not a leap lear";
+// }
+    return 0;
+}
