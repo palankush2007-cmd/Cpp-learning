50ad4bd3bcb481b9b998f2db4cfe685d85f46971
Ankush pal <palankush2007@gmail.com>
2026-05-22 14:50:50 +0530

    variables and datatypes (VandDT)

---
+++ VandDT.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+// variables and datatypes (VandDT)
+int main() {
+    // int n;
+    // int area;
+    // cout<<"The side of a square:\n";
+    // cin>>n;
+    // area=n*n;
+    // cout<<"Area of square:"<<area<<endl;
+// float  object1, object2, object3;
+// float total;
+// cout<<"price of object1:";
+// cin>>object1;
+// cout<<"price of object2:";
+// cin>>object2;
+// cout<<"price of object3:";
+// cin>>object3;
+// total= object1+object2+object3;
+// float PA=0.18*total;
+// float BILL=PA+total;
+// cout<<"total bill withGST:"<<BILL<<endl;
+    //  float PA, Rate, Time,SI;
+    //  cout<<"enter the principal amount:";
+    //  cin>>PA;
+    //  cout<<"enter the rate:";
+    //  cin>>Rate;
+    //  cout<<"enter the time (in years):";
+    //  cin>>Time;
+    // SI=(PA*Rate*Time)/100;
+    // cout<<"the SI is:"<<SI<<endl;
+// float radius;
+// float Area;
+// cout<<"Enter the radius of circle:";
+// cin>>radius;
+// Area= PI * radius*radius;
+// cout<<Area;
+    return 0;
+}
