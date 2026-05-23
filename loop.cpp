4f10fdc7035dd9b8b808d50fba054d495a1448ac
Ankush pal <palankush2007@gmail.com>
2026-05-23 10:26:32 +0530

    loop

---
+++ loop.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    // int num;
+    // cout<<"Enter a number: ";
+    // cin>>num;
+    // if(num==0 || num==1){
+    //     cout<<"the factorial is 1"<<endl ;
+    // }else if(num<0){
+    //     cout<<"The factorial of negative number not exist "<<endl ;
+    // }else{
+    //     int fact=1;
+    //     for(int i=1;i<=num;i++){
+    //         fact*=i;
+    //     }
+    //     cout<<"The factorial of the number is ";cout<< fact << endl ;
+    // }
+
+    
+// int num;
+// cout<<"Enter the number:";
+// cin>>num;
+// for(int i=1; i<=10; i++){
+//    cout<< num*i <<endl; }
+
+
+// int num;
+// cout<<"Enter a number:";
+// cin>>num;
+// int digit=0;
+// int temp=num;
+// int remainder;
+// int result;
+// while(temp!=0){
+//     temp=temp/10;
+//    digit++;
+// }
+// cout<<digit;
+// while(temp != 0) {
+//         remainder = temp % 10;
+//         result = result + pow(remainder, digits);
+//         temp = temp / 10;
+//     }
+//     if(result == num) {
+//         cout << num << " is an Armstrong number";
+//     }
+//     else {
+//         cout << num << " is not an Armstrong number";
+//     }
+
+
+    // int num;
+    // bool isprime=true;
+    // cout<<"Enter a number:";
+    // cin>>num;
+    // for(int i=2;i<=num-1;i++){
+    //     if(num%i==0){
+    //       isprime=false;
+    //       break;
+    //     }
+    // } 
+    // if(isprime){
+    //     cout<<"number is prime number";
+    // }else{
+    //     cout<<"number is not a prime";
+    // }  
+
+// int num;
+// int a=0,b=1;
+// int c;
+// cout<<"Enter a number:";
+// cin>>num;
+// for(int i=2; i=num;i++){
+//      c=a+b;
+//      cout<< c <<endl;
+//      a=b;b=c;
+// } 
+return 0;   
+};
